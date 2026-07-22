fb25ff1543919c50eae49473477c6c198efd7afd
shahzadiqbal <shahzadiqbal.may18@gmail.com>
2026-07-22 20:49:51 +0500

    array learnign class

---
+++ batch2026/src/ArrayLearning.java
@@ -0,0 +1,9 @@
+public class ArrayLearning {
+
+    public static void main(String[] args) {
+        int[] arr = {1,4,6,7};
+        for(int el : arr){
+            System.out.println(el);
+        }
+    }
+}
