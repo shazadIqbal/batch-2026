5cc5efbe1367ea892183b9efff99f0a2caf5a0cb
munizaaftab-1 <muniza.aftab99@gmail.com>
2026-07-22 21:07:18 +0500

    Reverse Array Task

---
+++ batch2026/src/ReverseArray.java
@@ -0,0 +1,10 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,7,4};
+
+        for (int i = arr.length -1; i >= 0; i--) {
+            System.out.println(arr[i]);
+
+        }
+    }
+}
