846d5929f818e02c1c625ef8cfa4a53d1030fab3
ahsannnaeem <ahsan.naeem118@gmail.com>
2026-07-22 21:10:37 +0500

    reverse

--- batch2026/src/Ahsan.java
+++ batch2026/src/Ahsan.java
@@ -4,8 +4,11 @@ public class Ahsan {
         int sum=0;
         for (int el : arr){
             sum+=el;
-
+        }
+        for (int i = arr.length-1; i >= 0; i--) {
+            System.out.println(arr[i]);
         }
         System.out.println(sum);
+
     }
 }
