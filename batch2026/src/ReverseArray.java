b76fca877171235a238e0a896da8df11525a5026
kabir745 <bsse2280171@szabist.pk>
2026-07-22 21:10:03 +0500

    new class

---
+++ batch2026/src/ReverseArray.java
@@ -0,0 +1,9 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {10,24,31,43,22};
+        for (int i = arr.length - 1; i >=0; i--) {
+            System.out.println(arr[i]);
+
+        }
+    }
+}
