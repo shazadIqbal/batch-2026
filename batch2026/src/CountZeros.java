d64a2b5d4a6e3b1757ca97609fc7acfecf53308c
munizaaftab-1 <muniza.aftab99@gmail.com>
2026-07-24 19:21:45 +0500

    tasks

--- batch2026/src/CountZeros.java
+++ batch2026/src/CountZeros.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class CountZeros {
-}
+    public static void main(String[] args) {
+
+        int[] arr = {1,4,0,2,3,0,9,8,0,0,1,0,0};
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        }
+
+    }
--- batch2026/src/TestingSplitFunction.java
+++ batch2026/src/TestingSplitFunction.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class TestingSplitFunction {
+    public static void main(String[] args) {
+
+        String sentence = "I am learning Java";
+        String[] words  = sentence.split(" ");
+
+        for (String word: words){
+            System.out.println(word);
+
+        }
+
+    }
 }
